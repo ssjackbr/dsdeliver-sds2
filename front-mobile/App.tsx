f540a0d15c8b8b7792688d148ef522baec8b33e5
Jackson Rodrigues <jjr0910@gmail.com>
2021-01-16 13:17:00 -0300

    Add orders list

--- front-mobile/App.tsx
+++ front-mobile/App.tsx
@@ -2,9 +2,8 @@ import { useFonts, OpenSans_400Regular, OpenSans_700Bold } from '@expo-google-fo
 import AppLoading from 'expo-app-loading';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Header from './src/Header';
-import Home from './src/Home';
+import { StyleSheet, View } from 'react-native';
+import Routes from './src/Routes';
 
 export default function App() {
 
@@ -20,8 +19,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <Header />
-      <Home />
+      <Routes/>
     </View>
   );
 }
--- front-mobile/src/Home/index.tsx
+++ front-mobile/src/Home/index.tsx
@@ -1,14 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
+import { useNavigation } from '@react-navigation/native';
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
+import Header from '../Header';
 
  function Home() {
+    const navigation = useNavigation();
 
-    const handleOnPress = () => {}
-
+    const handleOnPress = () => {
+      navigation.navigate('Orders');
+    }
+    
   return (
-    <> 
+    <>
+   <Header />
     <View style={styles.container}>
         <Image source={require('../assets/deliveryman.png')}  />
         <Text style={styles.title}>Acompanhe os pedidos e {'\n'} entregue no prazo!</Text>
@@ -16,7 +21,7 @@ import { RectButton } from 'react-native-gesture-handler';
     </View>
 
     <View style={styles.footer}> 
-        <RectButton style={styles.button}>
+        <RectButton style={styles.button} onPress={handleOnPress}>
             <Text style={styles.buttonText}> VER PEDIDOS</Text>
         </RectButton>
      </View>
